1d612d3917b3f1fc31b0a12e6b63813df95fa139
jake8989 <jjk848798@gmail.com>
2024-12-12 19:31:28 +0530

    dummy pdf report

--- app/services/ReportService.py
+++ app/services/ReportService.py
@@ -3,16 +3,36 @@ from dotenv import load_dotenv
 import asyncio
 from app.gRPC import reports_pb2_grpc, reports_pb2
 from app.db.config import database
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML, CSS
 
 load_dotenv()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = BASE_DIR / "template"
+STATIC_DIR = BASE_DIR / "static"
+SAVE_DIR = BASE_DIR / "reports"
+# print(BASE_DIR, TEMPLATE_DIR, STATIC_DIR)
+
+templateEnv = Environment(loader=FileSystemLoader(str(TEMPLATE_DIR)))
 
 
 class ReportService(reports_pb2_grpc.ReportServiceServicer):
     async def GenerateReport(self, request, context):
         print("Generating Reports...")
-        users = await database.db["users"].find({}).to_list()
-        print(users)
+
         print(request)
+        template = templateEnv.get_template("report.html")
+        html_content = template.render()
+        # print(html_content)
+        pdf_report_content = HTML(string=html_content).write_pdf()
+        pdf_saved_dir = SAVE_DIR / f"{request.username}_report.pdf"
+
+        with open(pdf_saved_dir, "wb") as f:
+            f.write(pdf_report_content)
+
         return reports_pb2.ReportResponse(
             success=True, message="Correted!", monthly_report=b"Hii"
         )
--- generate.sh
+++ generate.sh
@@ -1,1 +1,3 @@
 python -m grpc_tools.protoc -I=app/gRPC --python_out=app/gRPC --grpc_python_out=app/gRPC app/gRPC/otp.proto
+
+PYTHONPATH="/media/jayant/New Volume/TS/finbudreports" watchmedo auto-restart --patterns="*.py" --recursive -- python app/main.py
